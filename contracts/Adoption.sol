72f3bd13138cd26a1d099026f886e14a69a56fdf
Anthony I. Stewart <anthony@stewart-technologies.com>
2019-12-09 18:49:30 -0500

    Updated adoption.sol
    Added getAdopter function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,26 +1,47 @@
 pragma solidity ^0.5.0;
 
 contract Adoption{
-  address[16] public adopters;
 
-  // Create a variable that stores the pet price
-  uint private petPrice = 1 ether;
+  struct Pet {
+    uint petPrice;
+    address owner;
+  }
+
+  mapping(uint => Pet) pets;
+  //Pet pets[16];
+
+  constructor() public{
+      for(uint i=0; i<16; i++){
+          pets[i].petPrice = 1 ether;
+          pets[i].owner = address(0);
+      }
+  }
 
   // Adopting a pet
   // This function becomes "payable" so we can send money to the contracts
   // We also require the value sent to this contract be exactly equal to the pet price
   function adopt(uint petId) payable public returns (uint){
     require(petId >= 0 && petId <=15, "Invalid Pet ID");
-    require(adopters[petId] == address(0), "Pet already owned.");
-    require(msg.value == petPrice, "You must pay the exact price.");
+    require(pets[petId].owner == address(0), "Pet already owned.");
+    require(msg.value == pets[petId].petPrice, "You must pay the exact price.");
 
-    adopters[petId] = msg.sender;
+    pets[petId].owner = msg.sender;
 
     return petId;
-}
+  }
 
-  // Retrieving the adopters
+  // Retrieving the address of the owner of this pet
+  function getAdopter(uint petId) public view returns (address){
+    require(petId >= 0 && petId <=15, "Invalid Pet ID");
+    return pets[petId].owner;
+  }
+
+  // Retrieving all the adopters
   function getAdopters() public view returns (address[16] memory){
+    address[16] memory adopters;
+    for(uint i=0; i<16; i++){
+      adopters[i] = pets[i].owner;
+    }
     return adopters;
   }
 }
